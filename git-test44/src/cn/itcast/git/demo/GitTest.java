d70143d5815ad2e38982c1066d981964ee45d497
zhangjw2436@163.com <jiawei77520>
2018-07-06 19:28:36 +0800

    提交

---
+++ git-test44/src/cn/itcast/git/demo/GitTest.java
@@ -0,0 +1,11 @@
+package cn.itcast.git.demo;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+    }
+}
